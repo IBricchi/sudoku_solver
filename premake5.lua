809faa1b618fb20ca32cc80da2b0784baa7ef331
IBricchi <ignacio.bricchi@gmail.com>
2021-05-06 17:02:06 +0200

    setup simple program with glfw and glad

---
+++ premake5.lua
@@ -0,0 +1,49 @@
+workspace "sudoku_solver"
+    configurations { "debug", "release" }
+    platforms { "win64", "linux64"}
+
+    defaultplatform "win64"
+
+    filter "platforms:win64"
+        system "windows"
+        architecture "x86_64"
+
+    filter "platforms:linux64"
+        system "linux"
+        architecture "x86_64"
+
+outputdir = "%{cfg.buildcfg}/%{cfg.platform}"
+
+include "vendor/GLFW/premake5.lua"
+include "vendor/glad/premake5.lua"
+
+project "sudoku_solver"
+    kind "ConsoleApp"
+    language "C++"
+
+    dependson "GLFW"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin/obj/" .. outputdir .. "/%{prj.name}")
+
+    files { "%{wks.location}/src/**.h", "%{wks.location}/src/**.cpp" }
+ 
+    includedirs {
+        "%{wks.location}/vendor/GLFW/include/",
+        "%{wks.location}/vendor/glad/include/",
+    }
+
+    filter "platforms:win64"
+        staticruntime "on"
+        links {
+            "GLFW",
+            "OpenGL32.lib"
+        }
+
+    filter "configurations:debug"
+       defines { "DEBUG" }
+       symbols "On"
+ 
+    filter "configurations:release"
+       defines { "NDEBUG" }
+       optimize "On"
---
+++ vendor/glad/premake5.lua
@@ -0,0 +1,32 @@
+project "glad"
+	kind "StaticLib"
+	language "C"
+	architecture "x86_64"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin/obj/" .. outputdir .. "/%{prj.name}")
+	
+	includedirs { "include/" }
+
+    files{
+        "src/glad.c",
+        "src/glad.c",
+    }
+
+    filter "system:linux"
+        pic "On"
+
+        systemversion "latest"
+        staticruntime "On"
+
+    filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
